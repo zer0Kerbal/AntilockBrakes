5c9346fb5db2bf8d01ebcecc816ed45236ead470
puzzler995 <emarsee1@outlook.com>
2014-06-04 23:25:13 -0400

    Added rudementary Electricty support as well as changed from System.Timers to a timer based on KSP's Time

--- AntiLockBrakes/AntiLockBrakes/Plugins/ABSPart.cs
+++ AntiLockBrakes/AntiLockBrakes/Plugins/ABSPart.cs
@@ -6,7 +6,11 @@ namespace AntiLockBrakes
     public class ABSPart : PartModule
     {
         Timer timer;
+        float timesince = 0;
         Boolean running = false;
+
+        [KSPField(isPersistant = false)]
+        public float PowerConsumption;
 
 
         public override void OnStart(StartState state)
@@ -15,20 +19,47 @@ namespace AntiLockBrakes
 
             if (state != StartState.Editor)
             {
-                timer = new Timer(100);
-                print("ABS: Timer Set");
-                timer.Elapsed += timer_Elapsed;
-                print("ABS: Timer Elapsed Set");
+                //timer = new Timer(100);
+                //print("ABS: Timer Set");
+                //timer.Elapsed += timer_Elapsed;
+                //print("ABS: Timer Elapsed Set");
             }
         }
 
         public override void OnUpdate()
         {
-            base.OnUpdate();
+            if (running && !(vessel.GetSrfVelocity().magnitude <= 1))
+            {
+                if (GetPower(part, PowerConsumption))
+                {
+                    timesince += TimeWarp.deltaTime;
+                    print("ABS: " + timesince.ToString());
+                    if (timesince >= .1)
+                    {
+                        vessel.ActionGroups.ToggleGroup(KSPActionGroup.Brakes);
+                        print("ABS: Toggle Brakes");
+                        timesince = 0;
+                    }
+                }
+            }
+
             if (running && vessel.GetSrfVelocity().magnitude <= 1)
             {
                 Deactivate();
                 vessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+            }
+        }
+
+        private bool GetPower(Part part, float PowerConsumption)
+        {
+            if (TimeWarp.deltaTime != 0)
+            {
+                float amount = part.RequestResource("ElectricCharge", PowerConsumption * TimeWarp.deltaTime);
+                return amount != 0;
+            }
+            else
+            {
+                return true;
             }
         }
 
@@ -41,7 +72,7 @@ namespace AntiLockBrakes
         [KSPEvent(guiActive=true, guiName="Activate ABS")]
         public void Activate()
         {
-            timer.Start();
+            //timer.Start();
             print("ABS: Timer Started");
             running = true;
             Events["Activate"].active = false;
@@ -51,7 +82,7 @@ namespace AntiLockBrakes
         [KSPEvent(guiActive=true, guiName="Deactivate ABS", active=false)]
         public void Deactivate()
         {
-            timer.Stop();
+            //timer.Stop();
             print("ABS: Timer Stopped");
             running = false;
             Events["Activate"].active = true;
